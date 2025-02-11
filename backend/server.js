1e28dde6e48ccbbcda2b3dae50bbeeeba8024c30
Sourav <souravbhardwaj997@gmail.com>
2025-02-11 10:01:13 +0530

    Configuration for the deployment

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,18 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json());
 
 app.use("/api/products", productRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend", "dist")));
+  app.get("*", (req, res) => {
+    return res.send(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectToDB();
